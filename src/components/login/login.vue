0f0c60bf2be11dca903fa897d577fd853d1d6af2
li-ming-hui <358166192@qq.com>
2018-04-10 20:33:07 +0800

    登陆成功跳转到home组件

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,6 +1,25 @@
 <template>
  <div>
    <p>登录组件</p>
+   <form @submit.prevent="login">
+     <div>
+       <label for="username">用户名</label>
+       <input
+         type="text"
+         id="username"
+         v-model="userForm.username">
+     </div>
+     <div>
+       <label for="password">密码</label>
+       <input
+         type="password"
+         id="password"
+         v-model="userForm.password">
+     </div>
+     <div>
+       <button>点击登陆</button>
+     </div>
+   </form>
  </div>
 </template>
 
@@ -8,11 +27,26 @@
  export default {
    data () {
      return {
-
+       userForm:{
+         username: '',
+         password: ''
+       }
      }
    },
-   components: {
-
+   methods: {
+     async login () {
+       // 1. 采集表单数据
+       // 2. 表单验证
+       // 3. 发请求执行登陆操作
+       // 4. 根据响应做交互
+       const res =await axios.post('http://localhost:8888/api/private/v1/login', this.userForm)
+       const data = res.data
+       if(data.meta.status ===2000){
+         this.$router.push({
+           name: 'home'
+         })
+       }
+     }
    }
  }
 </script>
