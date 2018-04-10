6f181d0d486de08340d61f29f270f76a9394b4cc
li-ming-hui <358166192@qq.com>
2018-04-10 20:01:22 +0800

    添加登陆和home组件并配置路由导航

---
+++ src/components/login/login.vue
@@ -0,0 +1,23 @@
+<template>
+ <div>
+   <p>登录组件</p>
+ </div>
+</template>
+
+<script>
+ export default {
+   data () {
+     return {
+
+     }
+   },
+   components: {
+
+   }
+ }
+</script>
+
+<style>
+
+ 
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import Login from '@/components/login/login'
+import Home from '@/component/home/home'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
+      name: 'login',
+      path: '/login',
+      component: Login
+    },
+    {
+      name: 'home',
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      component: Home
     }
   ]
 })
