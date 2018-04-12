1dc60ba0cbc3ece05a78cd1226328b2301f97aa6
li-ming-hui <358166192@qq.com>
2018-04-12 14:21:13 +0800

    :lipstick:使用Element结合自定义样式处理登陆组件和Home组件的布局样式

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,7 +1,16 @@
 <template>
-<div>
-  <p>home component</p>
-</div>
+  <el-container class="container">
+    <el-header class="header">
+      <el-row>
+        <el-col :span="4"><div class="grid-content bg-purple"><img src="./logo.png" alt=""></div></el-col>
+        <el-col :offset="16" :span="4"><div class="grid-content bg-purple"><a href="#">退出</a></div></el-col>
+      </el-row>
+    </el-header>
+    <el-container class="container">
+      <el-aside class="aside" width="200px">Aside</el-aside>
+      <el-main class="main">Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -18,5 +27,24 @@ export default {
 </script>
 
 <style>
+.container {
+  height: 100%;
+  line-height: 60px;
+}
 
+.header {
+  background-color: #B3C0D1;
+}
+.container .header .el-row a {
+  float: right;
+  margin: 0 20px;
+}
+.aside {
+  background-color: #D3DCE6;
+}
+
+.main {
+  background-color: #E9EEF3;
+  height: 100%;
+}
 </style>
--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,6 +1,7 @@
 <template>
 <div class="login-wrap">
-  <el-form label-position="top" ref="form" :model="userForm" label-width="80px">
+  <el-form class="login-from" label-position="top" ref="form" :model="userForm" label-width="80px">
+    <h2 class="heading">用户登陆</h2>
     <el-form-item label="用户名">
       <el-input
         v-model="userForm.username"></el-input>
@@ -11,8 +12,7 @@
        v-model="userForm.password"></el-input>
     </el-form-item>
     <el-form-item>
-      <el-button type="primary" @click="login">立即创建</el-button>
-      <el-button>取消</el-button>
+      <el-button class="login-btn" type="primary" @click="login">立即登陆</el-button>
     </el-form-item>
   </el-form>
 </div>
@@ -37,7 +37,6 @@ export default {
       // 3. 发请求执行登陆操作
       // 4. 根据响应做交互
       const res = await axios.post('http://localhost:8888/api/private/v1/login', this.userForm)
-      console.log(res)
       const data = res.data
       if (data.meta.status === 200) {
         this.$router.push({
@@ -50,5 +49,27 @@ export default {
 </script>
 
 <style>
+.login-wrap {
+  background-color: #324152;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 
+.login-wrap .login-from {
+  background-color: #fff;
+  width: 400px;
+  padding: 30px;
+  border-radius: 5px;
+}
+
+.login-wrap .login-from .heading {
+  text-align: center;
+  color: skyblue;
+}
+
+.login-wrap .login-from .login-btn {
+  width: 100%;
+}
 </style>
