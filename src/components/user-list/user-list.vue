8998477178c7f6da8eb0cd8eb2bb8cd6cfa95504
li-ming-hui <358166192@qq.com>
2018-04-12 22:31:56 +0800

    完成用户列表组件布局

--- src/components/user-list/user-list.vue
+++ src/components/user-list/user-list.vue
@@ -1,23 +1,98 @@
 <template>
- <div>
-   <p>用户列表组件</p>
+ <div class="user-list-wrap">
+   <el-row>
+     <el-col>
+       <el-breadcrumb separator-class="el-icon-arrow-right">
+          <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+          <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+          <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+       </el-breadcrumb>
+     </el-col>
+   </el-row>
+   <el-row class="user-list-search">
+    <el-col :span="8">
+      <el-input placeholder="请输入内容" v-model="input5" class="input-with-select">
+        <el-button slot="append" icon="el-icon-search"></el-button>
+      </el-input>
+    </el-col>
+    <el-col :span="2">
+      <el-button type="success" plain>添加用户</el-button>
+    </el-col>
+  </el-row>
+  <el-table
+    :data="tableData"
+    style="width: 100%">
+    <el-table-column
+      prop="date"
+      label="日期"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="name"
+      label="姓名"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="address"
+      label="地址">
+    </el-table-column>
+  </el-table>
+  <el-pagination
+    @size-change="handleSizeChange"
+    @current-change="handleCurrentChange"
+    :current-page="4"
+    :page-sizes="[100, 200, 300, 400]"
+    :page-size="100"
+    layout="total, sizes, prev, pager, next, jumper"
+    :total="400">
+  </el-pagination>
  </div>
-</template>
+ </template>
 
 <script>
 
 export default {
   data () {
     return {
-
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }]
     }
   },
-  components: {
-
+  methods: {
+    handleSizeChange (val) {
+      console.log(`每页 ${val} 条`)
+    },
+    handleCurrentChange (val) {
+      console.log(`当前页: ${val}`)
+    }
   }
 }
 </script>
 
 <style>
+.user-list-breadcrumb {
+  line-height: 3;
+}
 
+.user-list-search {
+  margin: 5px 0;
+}
+.el-pagination {
+  margin-top: 15px;
+}
 </style>
