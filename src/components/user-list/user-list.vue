1d58ac2e1ef3a9e310e44823b38c69996049cc4d
li-ming-hui <358166192@qq.com>
2018-04-12 22:37:43 +0800

    使用axios配置请求头携带token令牌请求用户列表数据并渲染到表格组件中

--- src/components/user-list/user-list.vue
+++ src/components/user-list/user-list.vue
@@ -11,7 +11,7 @@
    </el-row>
    <el-row class="user-list-search">
     <el-col :span="8">
-      <el-input placeholder="请输入内容" v-model="input5" class="input-with-select">
+      <el-input placeholder="请输入内容" v-model="searchText" class="input-with-select">
         <el-button slot="append" icon="el-icon-search"></el-button>
       </el-input>
     </el-col>
@@ -23,18 +23,18 @@
     :data="tableData"
     style="width: 100%">
     <el-table-column
-      prop="date"
-      label="日期"
-      width="180">
-    </el-table-column>
-    <el-table-column
-      prop="name"
+      prop="username"
       label="姓名"
       width="180">
     </el-table-column>
     <el-table-column
-      prop="address"
-      label="地址">
+      prop="email"
+      label="邮箱"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="mobile"
+      label="电话">
     </el-table-column>
   </el-table>
   <el-pagination
@@ -50,27 +50,25 @@
  </template>
 
 <script>
-
+import axios from 'axios'
 export default {
+  async created () {
+    const {token} = JSON.parse(window.localStorage.getItem('admin-token'))
+    const res = await axios.get('http://localhost:8888/api/private/v1/users', {
+      headers: {
+        Authorization: token // 配置请求头携带身份令牌
+      },
+      params: { // 请求参数，对象会被转换为 k=v&k=v 的格式，然后拼接到请求路径 ? 后面发起请求
+        pagenum: 1,
+        pagesize: 5
+      }
+    })
+    this.tableData = res.data.data.users
+  },
   data () {
     return {
-      tableData: [{
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-04',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1517 弄'
-      }, {
-        date: '2016-05-01',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1519 弄'
-      }, {
-        date: '2016-05-03',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1516 弄'
-      }]
+      searchText: '',
+      tableData: []
     }
   },
   methods: {
